b85bd0730b6465f74f2e88a920cb6ae81f29937b
Dimitrios Sideris <76185385+Kroustalo@users.noreply.github.com>
2021-04-25 20:57:35 +0300

    Added split function Optimized p10 printing of binary characters

--- SDES-1390/Main.c
+++ SDES-1390/Main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <math.h>
 
+
+
 /*
 • Είσοδος:   00010101
 • Κλειδί:  0101101000
@@ -13,11 +15,16 @@
 
 //Prototypes
 int P10_perm(int key); // function to calclulate P10
+void Split_10(int n, int* split); //Spilt 10 bit key to two 5 bit ones in half
+static inline int bin(int bits, int shift); //aux function for packing shi(f)t commands
+void print_bin(int n, int bits);//print in bin
+
 
 int main(void) {
 
     int input, key, output;
-    
+    int split[2];
+
     input = 0b00010101;
     key = 0b1010000010; //642
           // 0b1000001100 524
@@ -25,30 +32,63 @@ int main(void) {
 
     key=P10_perm(key);
 
-
-    if (key == -1) {
+    if (key == -1) { //saftey
         return 1;
     }
 
+
     printf("\nPERM: %d\n", key);
+    print_bin(key, 10);
+
+    Split_10(key, split);
+    printf("split1: %d\n", split[0]);
+    printf("split2: %d\n", split[1]);
+
+
+
+    
+
+
+
     return 0;
 
 }
 
 
+//aux function for packing shi(f)t commands
+static inline int bin(int bits,int shift) {
+    return (1 << bits-1) >> shift;
+}
+
+//print in bin
+void print_bin(int n,int bits){
+    unsigned i;
+    for (i = 1 << bits-1; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
+    printf("\n");
+}
+
+
 
 //function to calclulate P10
-int P10_perm(int key) { 
+int P10_perm(int key) {
     if (key > 1023) {
         printf("value is greater than 10 bits");
         return -1;
     }
 
     int temp = 0;
-    int P10_GUIDE[10] = { 3,7,9,5,8,0,6,1,2,4}; //Shift positions according to pdf
+    int P10_GUIDE[10] = { 3,7,9,5,8,0,6,1,2,4 }; //Shift positions according to pdf
     for (int i = 0; i < 10; i++) {
-        temp += (key & (0b1000000000 >> i)) / (0b1000000000 >> i) * pow(2, P10_GUIDE[i]);
+        temp += (key & bin(10, i)) / (bin(10, i)) * 1 << P10_GUIDE[i];
     }
     return temp;
 
+}
+
+//Spilt 10 bit key to two 5 bit ones in half
+void Split_10(int n, int* split) { //Spilt 10 bit key to two 5 bit ones in half
+    *split = (0b1111100000 & n) >> 5;
+    split++;
+    *split = 0b0000011111 & n;
 }
