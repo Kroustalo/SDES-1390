9e346b531c7401ed28aca5db870d8ed1cd0ed5aa
Dimitrios Sideris <76185385+Kroustalo@users.noreply.github.com>
2021-04-28 12:35:14 +0300

    made get k1 and k2 function hacking function

--- SDES-1390/Main.c
+++ SDES-1390/Main.c
@@ -24,17 +24,21 @@ int ER_perm(int right); //function to calclulate ER perm
 int grid(int S, int array[4][4]); //extract elements from array
 int P4_perm(int parts[2]); //P4 perm after array
 
-void process(int LR[2], int k, int* output); //process function
+int IP_REV_perm(int message); // function to calclulate REVERSED IP PERM
+
+int init(int key, int* k1, int* k2); //Gets k1 and k2 from key
+int process(int LR[2], int k, int* output); //process function
 int encrypt(int message, int k1, int k2);  //encrypt
 int decrypt(int message, int k1, int k2);  //decrypt
-int IP_REV_perm(int message); // function to calclulate REVERSED IP PERM
+
+int hack(int message, int encrypted); //bruteforce all keys to find the right ones
+
 
 int main(void) {
 
     int  key, message, output, k1,k2; 
-    int split[2];
    
-    int option = 1; //encrypt 1 | decrypt 2 | hack 3
+    int option = 3; //encrypt 1 | decrypt 2 | hack 3
 
     message = 0b11110011;
     key = 0b1010000010; //642
@@ -50,81 +54,23 @@ int main(void) {
     key = 550;
     
     //message
-    printf("8bit message:\nDecimal: %d\nBinary:", message);
+    printf("8bit message:\nDecimal: %d\nBinary: ", message);
     print_bin(message, 8);
 
     //OG key
-    printf("\n10-bit key:\nDecimal: %d\nBinary:", key);
+    printf("\n10-bit key:\nDecimal: %d\nBinary: ", key);
     print_bin(key, 10);
-    
-    //P10
-    key = P10_perm(key);
-
-    if (key == -1) { //saftey
+   
+    if (init(key,&k1,&k2) == -1) { //saftey
         return 1;
     }
-    /*
-    printf("\nP10: %d\n", key);
-    print_bin(key, 10);
-    */
-
-    //SPLIT 10
-    Split(key, 10, split);
-    /*
-    printf("\nsplit1: %d\n", split[0]);
-    print_bin(split[0], 5);
-    printf("split2: %d\n", split[1]);
-    print_bin(split[1], 5);
-    */
-
-    //K1
-    LS_1(split);
-    /*
-    printf("\nls1 shift1: %d\n", split[0]);
-    print_bin(split[0], 5);
-    printf("ls1 shift2: %d\n", split[1]);
-    print_bin(split[1], 5);
-    */
-    k1 = K_perm(split);
-    /*
-    printf("\nK1: %d\n", k1);
-    print_bin(k1, 8);
-    */
-
-    //K2
-    LS_1(split);
-    LS_1(split);
-    /*
-    printf("\nls2 shift1: %d\n", split[0]);
-    print_bin(split[0], 5);
-    printf("ls2 shift2: %d\n", split[1]);
-    print_bin(split[1], 5);
-    */
-
-    k2 = K_perm(split);
-    /*
-    printf("\nK2: %d\n", k2);
-    print_bin(k2, 8);
-    */
-
-    //ip
-    message = IP_perm(message);
-
-    if (message == -1) { //saftey
-        return 1;
-    }
-    /*
-    printf("\nIP: %d\n", message);
-    print_bin(message, 8);
-    */
-
 /*---------------------------------*/
     if (option == 1) {
         output = encrypt(message, k1, k2);
         if (output == -1) { //saftey
             return 1;
         }
-        printf("\nEncoded Message:\nDecimal: %d\nBinary:", output);
+        printf("\nEncoded Message:\nDecimal: %d\nBinary: ", output);
         print_bin(output, 8);
     }
 /*---------------------------------*/
@@ -134,13 +80,25 @@ int main(void) {
         if (output == -1) { //saftey
             return 1;
         }
-        printf("\nDecoded:\nDecimal %d\nBinary:", output);
+        printf("\nDecoded:\nDecimal %d\nBinary: ", output);
         print_bin(output, 8);
     }
 /*---------------------------------*/
     else {
-        //HACKERMAN
+        int encrypted = 131;
+        printf("\nI'm about to hack Your Mom!\n");
+        printf("\nEncoded Message:\nDecimal: %d\nBinary: ", encrypted);
+        print_bin(encrypted, 8);
+        
+        int check = hack(message, encrypted);
+        if (check == -1) //saftey
+            return 1;
+        else if(check == 0)
+            printf("\nI Hacked Your Mom!\n");
+        else if(check == 1)
+            printf("Your mom is so big, that's shes unhackable\n");
     }
+
     return 0;
 
 }
@@ -301,7 +259,7 @@ int P4_perm(int parts[2]) {
 
 
 //encrypting function
-void process(int LR[2], int k, int* LFR) {
+int process(int LR[2], int k, int* LFR) {
 
     int ER, XOROUT1;
 
@@ -322,7 +280,7 @@ void process(int LR[2], int k, int* LFR) {
     //ER
     ER = ER_perm(LR[1]);
     if (ER == -1) { //saftey
-        return 1;
+        return -1;
     }
     
     /*
@@ -361,7 +319,7 @@ void process(int LR[2], int k, int* LFR) {
     //P4
     p4 = P4_perm(parts);
     if (p4 == -1) { //saftey
-        return 1;
+        return -1; //not really worth it
     }
     
     /*
@@ -380,6 +338,8 @@ void process(int LR[2], int k, int* LFR) {
     */
     LFR++;
     *LFR = LR[1]; //right
+
+    return 0;
 }
 
 // function to calclulate REVERSED IP PERM
@@ -398,16 +358,83 @@ int IP_REV_perm(int message) {
     return temp;
 }
 
+//Gets k1 and k2 from key
+int init(int key, int *k1, int *k2) {
+    int split[2];
+    //P10
+    key = P10_perm(key);
 
+    if (key == -1) { //saftey
+        return -1;
+    }
+    /*
+    printf("\nP10: %d\n", key);
+    print_bin(key, 10);
+    */
+
+    //SPLIT 10
+    Split(key, 10, split);
+    /*
+    printf("\nsplit1: %d\n", split[0]);
+    print_bin(split[0], 5);
+    printf("split2: %d\n", split[1]);
+    print_bin(split[1], 5);
+    */
+
+    //K1
+    LS_1(split);
+    /*
+    printf("\nls1 shift1: %d\n", split[0]);
+    print_bin(split[0], 5);
+    printf("ls1 shift2: %d\n", split[1]);
+    print_bin(split[1], 5);
+    */
+    *k1 = K_perm(split);
+    /*
+    printf("\nK1: %d\n", k1);
+    print_bin(k1, 8);
+    */
+
+    //K2
+    LS_1(split);
+    LS_1(split);
+    /*
+    printf("\nls2 shift1: %d\n", split[0]);
+    print_bin(split[0], 5);
+    printf("ls2 shift2: %d\n", split[1]);
+    print_bin(split[1], 5);
+    */
+
+    *k2 = K_perm(split);
+    /*
+    printf("\nK2: %d\n", k2);
+    print_bin(k2, 8);
+    */
+}
 int encrypt(int message, int k1, int k2) {
     int output, LR[2];
     int enc1[2], enc2[2];
+
+    //ip perm
+    message = IP_perm(message);
+
+    if (message == -1) { //saftey
+        return -1;
+    }
+    /*
+    printf("\nIP: %d\n", message);
+    print_bin(message, 8);
+    */
+
     //SPLIT 8
     Split(message, 8, LR);
 
     //ENCRYPT 1
     //printf("\nENCRYPT #1\n");
-    process(LR, k1, enc1);
+    if (process(LR, k1, enc1) == -1) { //saftey
+        return -1;
+    }
+    
 
     //switch
     LR[0] = enc1[1];
@@ -415,7 +442,9 @@ int encrypt(int message, int k1, int k2) {
 
     //ENCRYPT 2
     //printf("\nENCRYPT #2\n");
-    process(LR, k2, enc2);
+    if (process(LR, k2, enc2) == -1) { //saftey
+        return -1;
+    }
 
     //OUTPUT
     output = 0;
@@ -430,20 +459,35 @@ int encrypt(int message, int k1, int k2) {
 int decrypt(int message, int k1, int k2) {
     int output, LR[2];
     int enc1[2], enc2[2];
+
+    //ip perm
+    message = IP_perm(message);
+
+    if (message == -1) { //saftey
+        return -1;
+    }
+    /*
+    printf("\nIP: %d\n", message);
+    print_bin(message, 8);
+    */
+
     //SPLIT 8
     Split(message, 8, LR);
 
     //DECRYPT 1
     //printf("\nDECRYPT #1\n");
-    process(LR, k2, enc1);
-
+    if (process(LR, k2, enc1) == -1) { //saftey
+        return -1;
+    }
     //switch
     LR[0] = enc1[1];
     LR[1] = enc1[0];
 
     //DECRYPT 2
     //printf("\nDECRYPT #2\n");
-    process(LR, k1, enc2);
+    if (process(LR, k1, enc2) == -1) { //saftey
+        return -1;
+    }
 
     //OUTPUT
     output = 0;
@@ -453,4 +497,30 @@ int decrypt(int message, int k1, int k2) {
     output = IP_REV_perm(output);
 
     return output;
+}
+
+//bruteforce all keys to find the right ones
+int hack(int message, int encrypted) {
+    int output,k1,k2;
+    int j = 1;
+    
+    int flag=0;
+
+    for (int i = 0; i < 1024; i++) {
+        if (init(i,&k1,&k2) == -1) { //saftey
+            return -1;
+        }
+        output=encrypt(message, k1, k2);
+        if (output == encrypted) {
+            printf("\n10-bit key Found (#%d):\nDecimal: %d\nBinary: ", j,i);
+            print_bin(i, 10);
+            j++;
+        }
+    }
+
+    if (flag) {
+        return 1;
+    }
+
+    return 0;
 }
